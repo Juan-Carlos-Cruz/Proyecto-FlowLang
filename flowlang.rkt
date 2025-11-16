730595bcac730773ff7d67396f46d7945a098ec3
Juan Carlos Cruz Muñoz <juan.cruz.munoz@correounivalle.edu.co>
2025-11-16 17:52:34 -0500

    Implementar evaluación básica y variables

--- flowlang.rkt
+++ flowlang.rkt
@@ -73,3 +73,87 @@
 
 (define (result v env store next)
   (values v env store next))
+
+(define primitive-symbols
+  '(+ - * / < <= > >= not and or equal? list-push!
+    vacio? crear-lista lista? cabeza cola append ref-list set-list
+    crear-diccionario diccionario? ref-diccionario set-diccionario
+    claves valores
+    longitud concatenar
+    % // add1 sub1))
+
+(define (primitive-symbol? sym)
+  (memq sym primitive-symbols))
+
+(define return-no-expr-tag '#:return-none)
+(struct return-signal (value) #:transparent)
+
+(define (make-binding-entry id expr) (list id expr))
+(define (binding-id entry) (car entry))
+(define (binding-expr entry) (cadr entry))
+
+(define (eval-var-bindings bindings env store next)
+  (let loop ((bs bindings) (e env) (s store) (n next))
+    (if (null? bs)
+        (result 'ok e s n)
+        (let* ([entry (car bs)]
+               [x (binding-id entry)]
+               [expr (binding-expr entry)])
+          (define-values (loc n1) (store-alloc s n 'null))
+          (define env-with-var (env-extend e x (binding loc #t)))
+          (define-values (v env1 s1 n2) (eval* expr env-with-var s n1))
+          (store-set! s1 loc v)
+          (loop (cdr bs) env1 s1 n2)))))
+
+(define (eval-program ast)
+  (define-values (S N) (make-empty-store))
+  (define-values (v _e _S _N) (eval* ast empty-env S N))
+  (if (return-signal? v) (return-signal-value v) v))
+
+(define (run ast)
+  (eval-program ast))
+
+(define (eval-list es env store next)
+  (let loop ((es es) (e env) (s store) (n next) (acc '()))
+    (if (null? es) (values (reverse acc) e s n)
+        (let-values ([(v e1 s1 n1) (eval* (car es) e s n)])
+          (loop (cdr es) e1 s1 n1 (cons v acc))))))
+
+(define (eval-seq es env store next new-scope?)
+  (let seq ((es es) (e env) (s store) (n next) (last 'null))
+    (if (null? es) (values last e s n)
+        (let-values ([(v1 e1 s1 n1) (eval* (car es) e s n)])
+          (if (return-signal? v1)
+              (values v1 e1 s1 n1)
+              (seq (cdr es) e1 s1 n1 v1))))))
+
+(define (eval-seq-flat es env store next)
+  (let seq ((es es) (e env) (s store) (n next) (last 'null))
+    (if (null? es) (values last e s n)
+        (let-values ([(v1 e1 s1 n1) (eval* (car es) e s n)])
+          (if (return-signal? v1)
+              (values v1 e1 s1 n1)
+              (seq (cdr es) e1 s1 n1 v1))))))
+
+(define (eval* ast env store next)
+  (match ast
+    [(list 'program es ...) (eval-seq es env store next #t)]
+    [(list 'block es ...)   (eval-seq es env store next #f)]
+    [(list 'begin es ...)   (eval-seq-flat es env store next)]
+
+    [(list 'var* bindings)
+     (eval-var-bindings bindings env store next)]
+
+    [(list 'var (? symbol? x) e)
+     (eval-var-bindings (list (make-binding-entry x e)) env store next)]
+
+    [(? (λ (x) (and (symbol? x) (not (eq? x 'null)))) x)
+     (if (primitive-symbol? x)
+         (result x env store next)
+         (let ([b (env-lookup env x)])
+           (result (store-ref store (binding-loc b)) env store next)))]
+
+    [(or (? number?) (? boolean?) (? string?) 'null)
+     (result ast env store next)]
+
+    [else (error 'eval (format "AST no reconocido: ~a" ast))]))
